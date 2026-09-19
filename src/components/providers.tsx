497d0b28e5b9efb1b32b700acec4982bd6f827c5
Rushi0508 <rushigandhi14@gmail.com>
2026-09-19 20:35:44 +0530

    feat(theme): default to dark theme for first-time visitors

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -7,7 +7,7 @@ import { TooltipProvider } from "@/components/ui/tooltip"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
       <TooltipProvider>{children}</TooltipProvider>
       <Toaster position="bottom-right" />
     </ThemeProvider>
